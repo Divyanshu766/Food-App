1c4f2528ed369ea1d40c8be9ec7d65ded34e60a5
Divyanshu766 <lalitarawat90123@gmail.com>
2023-10-22 00:00:12 +0530

    Day-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", {}, "I'm H1"),
   React.createElement("h2", {}, "I'm H2"),
