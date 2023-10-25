e7d37c96fef684e99a7edc9a29aa54332694cb2d
Divyanshu766 <lalitarawat90123@gmail.com>
2023-10-25 12:24:09 +0530

    Day-4

--- App.js
+++ App.js
@@ -1,11 +1,542 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", {}, "I'm H1"),
-  React.createElement("h2", {}, "I'm H2"),
-]);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-clipart/20230106/original/pngtree-simple-and-modern-food-logo-vector-design-png-image_8876455.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurentCard = (props) => {
+  const { name, cuisines, avgRating } = props?.resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          props?.resData?.info?.cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{props?.resData?.info?.sla?.slaString}</h4>
+    </div>
+  );
+};
+
+const resObject = [
+  {
+    info: {
+      id: "311806",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Chakrata Road",
+      areaName: "Ballupur",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "311806",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2000,
+      },
+      parentId: "166",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 2.7,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        lastMileTravelString: "2.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-chakrata-road-ballupur-dehradun-311806",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "78973",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Vasant Vihar",
+      areaName: "Balliwala",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.6,
+      feeDetails: {
+        restaurantId: "78973",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 1900,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 1900,
+      },
+      parentId: "721",
+      avgRatingString: "3.6",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 3.7,
+        serviceability: "SERVICEABLE",
+        slaString: "30 mins",
+        lastMileTravelString: "3.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-vasant-vihar-balliwala-dehradun-78973",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "418291",
+      name: "Pop's Kitchen",
+      cloudinaryImageId: "thbdgubtwilo4wetlokl",
+      locality: "Old Nehru Colony",
+      areaName: "Dharampur",
+      costForTwo: "₹350 for two",
+      cuisines: ["Indian", "Chinese", "Tandoor", "North Indian", "Thalis"],
+      avgRating: 3.8,
+      feeDetails: {
+        restaurantId: "418291",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2000,
+      },
+      parentId: "389943",
+      avgRatingString: "3.8",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 38,
+        lastMileTravel: 2.8,
+        serviceability: "SERVICEABLE",
+        slaString: "38 mins",
+        lastMileTravelString: "2.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pops-kitchen-old-nehru-colony-dharampur-dehradun-418291",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "84229",
+      name: "Punjabi Dhaba",
+      cloudinaryImageId: "yyrjmyajqhyjq6mp1gha",
+      locality: "Gandhi Road",
+      areaName: "Paltan Bazaar",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian"],
+      avgRating: 3.6,
+      feeDetails: {
+        restaurantId: "84229",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2000,
+      },
+      parentId: "1476",
+      avgRatingString: "3.6",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 1.1,
+        serviceability: "SERVICEABLE",
+        slaString: "21 mins",
+        lastMileTravelString: "1.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/punjabi-dhaba-gandhi-road-paltan-bazaar-dehradun-84229",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "328442",
+      name: "Rol Gol",
+      cloudinaryImageId: "itaqp0hzcrx0xnqisvzv",
+      locality: "Rajpur Road",
+      areaName: "Race Course",
+      costForTwo: "₹150 for two",
+      cuisines: ["Snacks"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "328442",
+        fees: [
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "BASE_DISTANCE",
+            fee: 2000,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2000,
+      },
+      parentId: "170654",
+      avgRatingString: "4.1",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 2.4,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "2.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/rol-gol-rajpur-road-race-course-dehradun-328442",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "333532",
+      name: "Sarovar Portico",
+      cloudinaryImageId: "ojccj2pdugzpcs8wpm5a",
+      locality: "Rajpur Road",
+      areaName: "Rajpur",
+      costForTwo: "₹600 for two",
+      cuisines: ["North Indian", "Asian", "Chinese"],
+      feeDetails: {
+        restaurantId: "333532",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2500,
+      },
+      parentId: "11775",
+      avgRatingString: "--",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 4.9,
+        serviceability: "SERVICEABLE",
+        slaString: "32 mins",
+        lastMileTravelString: "4.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-10-25 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/sarovar-portico-road-rajpur-dehradun-333532",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resObject.map((ele) => {
+          return <RestaurentCard key={ele.info.id} resData={ele} />;
+        })}
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
