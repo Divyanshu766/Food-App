06473ecf1b066c88e9bb74d46565781465840d23
Divyanshu766 <lalitarawat90123@gmail.com>
2023-11-30 01:32:11 +0530

    Final UI Update to Home Page

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -2,15 +2,12 @@ import { CDN_URL } from "../utils/constants";
 const RestaurentCard = (props) => {
   const { name, cuisines, avgRating, areaName } = props?.resData?.info;
   return (
-    <div
-      className="res-card m-4 p-4 w-[250px]"
-      style={{ backgroundColor: "#f0f0f0" }}
-    >
+    <div className="res-card m-4 p-4 w-[250px] h-[420px]  bg-gray-100 rounded-lg hover:bg-gray-200">
       <img
-        className="res-logo"
+        className="res-logo rounded-lg h-[200px] w-[250px]"
         src={CDN_URL + props?.resData?.info?.cloudinaryImageId}
       />
-      <h3>{name}</h3>
+      <h3 className="font-bold py-3 text-md">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>Rating : {avgRating} stars</h4>
       <h4>{props?.resData?.info?.sla?.slaString}</h4>
