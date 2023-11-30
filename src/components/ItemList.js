64354b76da0b62586e297968988569bdd7d5065f
Divyanshu766 <lalitarawat90123@gmail.com>
2023-11-30 13:47:11 +0530

    Add Data to Accordian

---
+++ src/components/ItemList.js
@@ -0,0 +1,20 @@
+const ItemList = ({ items }) => {
+  console.log(items);
+  return (
+    <div>
+      {items.map((item) => {
+        return (
+          <div key={item.card.info.id} className="p-2 m-2 border-b-2 text-left">
+            <div className="py-2">
+              <span>{item.card.info.name}</span>
+              <span>- ₹ {item.card.info.price / 100}</span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ItemList;
